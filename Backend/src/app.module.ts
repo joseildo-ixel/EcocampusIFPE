5b6ea1b8163c5f94299e9752d0f879f861453380
Joseildo dos santos <js14@discente.ifpe.edu.br>
2026-09-21 21:10:40 -0300

    feat: adiciona entidade Descarte e registra no AppModule

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ObjetosModule } from './objetos/objetos.module';
 import { ColetorModule } from './coletor/coletor.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+import { DescarteModule } from './Descarte/descarte.module';
 
 @Module({
-  imports: [ObjetosModule, ColetorModule, UsuariosModule],
+  imports: [ObjetosModule, ColetorModule, UsuariosModule, DescarteModule],
   controllers: [AppController],
   providers: [AppService],
 })
